6a4f2f5091381d673ed592c7142024b6460b2fe2
ekosarr <ekoafi123@gmail.com>
2024-06-16 15:17:25 +0700

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,12 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="template/assets/images/logos/logo.png" height="100" alt="">
+                <a href="">
+                  <img src="template/assets/images/baru/tombolback.png" height="50" alt="">
                 </a>
+                <p href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                  <img src="template/assets/images/logos/logo.png" width="100" alt="">
+                </p>
                 <form>
                   <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Username</label>
